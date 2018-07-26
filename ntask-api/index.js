e213e58148edb89e8a1c8b2d830db16261aa3ff1
thiagosonnessogarcia <thiagosonnesso1994@gmail.com>
2018-07-26 16:23:44 -0300

    Criando a fase de teste do projeto

--- ntask-api/index.js
+++ ntask-api/index.js
@@ -3,12 +3,13 @@ import consign from "consign";
 
 const app = express();
 
-consign()
+consign({verbose: false})
     .include("libs/config.js")
     .then("db.js")
     .then("auth.js")
-    .then("models")
     .then("libs/middlewares.js")
     .then("routes")
     .then("libs/boot.js")
     .into(app);
+
+    module.exports = app;
