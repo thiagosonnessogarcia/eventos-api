db268af352a0652821c7f3b8d64f5dafdf57bf84
thiagosonnessogarcia <thiagosonnesso1994@gmail.com>
2018-07-26 15:32:28 -0300

    Refactorings para garantia de utilização do ID do USER

--- ntask-api/routes/users.js
+++ ntask-api/routes/users.js
@@ -1,18 +1,19 @@
 module.exports = app => {
     const Users = app.db.models.Users;
-
-    app.get("/users/:id", (req, res) => {
-        Users.findById(req.params.id, {
-            attributes: ["id", "nome", "email"]
-        })
-        .then(result => res.json(result))
-        .catch(e => {
-            res.status(412).json({msg: e.message});
+    
+    app.route("/user")
+        .all(app.auth.authenticate())
+        .get((req, res) => {
+            Users.findById(req.user.id, {
+                attributes: ["id", "name", "email"]
+            })
+            .then(result => res.json(result))
+            .catch(e => {
+                res.status(412).json({msg: e.message});
         });
-    });
-
-    app.delete("/users/:id", (req, res) => {
-        Users.destroy({where: {id: req.params.id} })
+    })
+    .delete((req, res) => {
+        Users.destroy({where: {id: req.user.id} })
             .then(result => res.sendStatus(204))
             .catch(e => {
                res.status(412).json({msg: e.message});
