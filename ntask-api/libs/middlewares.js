0bdd2993170d679c0070bcdef03f615a20dd5be5
thiagosonnessogarcia <thiagosonnesso1994@gmail.com>
2018-07-27 16:33:58 -0300

    Refactoring Midd com COrs

--- ntask-api/libs/middlewares.js
+++ ntask-api/libs/middlewares.js
@@ -2,9 +2,15 @@
 
 import bodyParser from "body-parser";
 import express from "express";
+import cors from "cors";
 module.exports = app => {
     app.set("port", 3000);
     app.set("json spaces", 4);
+    app.use(cors({
+        origin: ["http://localhost:3001"],
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"]
+    }));
     app.use(bodyParser.json());
     app.use(app.auth.initialize());
     app.use((req, res, next) => {
