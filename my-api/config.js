6a57b9ff0bc3457b553f58164dbec396e89e90ae
thiagosonnessogarcia <thiagosonnesso1994@gmail.com>
2018-07-27 15:30:03 -0300

    Removi Pastas para melhor visualização do projeto

--- my-api/config.js
+++
@@ -1,6 +0,0 @@
-// config.js para adicionar itens de configuração JWT
-
-module.exports = {
-    jwtSecret: "MyS3cr3tK3Y", // serve para codifica e decodificar TOKENS
-    jwtSession: { session: false} // session informa que a API não ira gerenciar a sessão
-}
--- node-babel/config.js
+++
@@ -1,6 +0,0 @@
-export default (() => {
-    return {
-        "hostname": "127.0.0.1",
-        "port": 1337
-    }
-})()
