4f61677e2788ed16aa218396d2ccf6fb03032c0f
thiagosonnessogarcia <thiagosonnesso1994@gmail.com>
2018-07-28 17:46:56 -0300

    Implementando o Front-End

---
+++ ntask-web/src/components/user.js
@@ -0,0 +1,57 @@
+import NTask from "../ntask.js";
+import Template from "../templates/user.js";
+
+class User extends NTask {
+    constructor(body) {
+        super();
+        this.body = body;
+    }
+    render() {
+        this.renderUserData();
+    }
+    addEventListener(){
+        this.userCancelClick();
+    }
+    renderUserData() {
+        const opts = {
+            method: "GET",
+            url: `${this.URL}/user`,
+            json: true,
+            headers: {
+                authorization: localStorage.getItem("token")
+            }
+        };
+        this.request(opts, (err, resp, data) => {
+            if (err || resp.status === 412) {
+                this.emit("Error", err);
+            } else {
+                this.body.innerHTML = Template.render(data);
+                this.addEventListener();
+            }
+        });
+    }
+    userCancelClick() {
+        const button = 
+        this.body.querySelector("[data-remove-account]");
+        button.addEventListener("click", (e) => {
+            e.preventDefault();
+            if (confirm("Tem certeza que deseja excluir a conta?")) {
+                const opts = {
+                    method: "DELETE",
+                    url: `${this.URL}/user`,
+                    headers: {
+                        authorization: localStorage.getItem("token")
+                    }
+                };
+                this.request(opts, (err, resp, data) => {
+                    if (err || resp.status === 412) {
+                        this.emit("Remove-error", err);
+                    } else {
+                        this.emit("Remove-account");
+                    }
+                });
+            }
+        });
+    }
+}
+module.exports = User;
